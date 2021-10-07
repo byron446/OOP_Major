6da7a6928846645fc0425f5ddc55d58de3f89d67
Max <maxasemenov@gmail.com>
2021-10-07 14:31:37 +1030

    beginning user interface

--- main.cpp
+++ main.cpp
@@ -5,3 +5,14 @@
 #include "Student.h"
 #include <string>
 #include <iostream>
+using namespace std;
+
+int main() {
+
+
+// login interface
+    cout << "Welcome to the University of Adelaide Timetable interface." << endl;
+    cout << "Please select from one of the following options:" << endl;
+    cout << "1. Login" << endl;
+    cout << "2. Quit" << endl;
+}
