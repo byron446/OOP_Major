1005fe4ac46c9029e40b193f329ac76bba5b4437
Max <maxasemenov@gmail.com>
2021-10-07 14:39:46 +1030

    developing user interface

--- main.cpp
+++ main.cpp
@@ -8,11 +8,34 @@
 using namespace std;
 
 int main() {
-
+    int login;
 
 // login interface
     cout << "Welcome to the University of Adelaide Timetable interface." << endl;
     cout << "Please select from one of the following options:" << endl;
     cout << "1. Login" << endl;
     cout << "2. Quit" << endl;
+    cin >> login;
+
+// if the user doesnt select 1 or 2 for login or quit, make them select again
+    while (login != 1 || login != 2) {
+        cout << "Welcome to the University of Adelaide Timetable interface." << endl;
+        cout << "Please select from one of the following options:" << endl;
+        cout << "1. Login" << endl;
+        cout << "2. Quit" << endl;
+        cin >> login;
+    }
+
+// if they choose to Login
+    if (login == 1) {
+        cout << "Please enter your identification number to proceed with the login process:" << endl;
+        cout << "1. id:" << endl;
+    }
+
+// if they choose to Quit
+    if (login == 2) {
+        cout << "Shutting down session..." << endl;
+    }
+
+    return 0;
 }
