818db7a169943386520195dd1c43614ad408126f
byron446 <byronbettsrandom@gmail.com>
2021-10-13 09:47:50 +1030

    printgrades

--- Student.cpp
+++ Student.cpp
@@ -12,7 +12,8 @@ Student::Student()
 
 void Student::printGrades()
 {
+    std::cout << "Your grades in each subject are: " << std::endl;
     for(int i = 0; i < getCourses().size(); i++){
-        
+        std::cout << getCourses().at(i)->getCourseId();
     }
 }
