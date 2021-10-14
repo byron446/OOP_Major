44ef8b1b97f0ad7535eba83b7a647c0a88d6bfcc
Max <maxasemenov@gmail.com>
2021-10-14 13:39:08 +1030

    commenting out the things that include the other files

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 //This is the main file
-#include "Course.h"
-#include "Person.h"
-#include "Teacher.h"
-#include "Student.h"
+//#include "Course.h"
+//#include "Person.h"
+//#include "Teacher.h"
+//#include "Student.h"
 #include <string>
 #include <iostream>
 #include <vector>
@@ -36,19 +36,19 @@ int main() {
 
         // if they are a teacher
         if (idcheck <= 15000) {
-            Teacher Teacher1; // defining teacher type
+            //Teacher Teacher1; // defining teacher type
             int teachchoice = 0;
             while (teachchoice != 1 || teachchoice != 2 || teachchoice != 3) {
-                std::cout << "Welcome " << Teacher1.getName() << std::endl; // giving options to the teacher
+                //std::cout << "Welcome " << Teacher1.getName() << std::endl; // giving options to the teacher
                 std::cout << "1. View Timetable" << std::endl;
                 std::cout << "2. View Grades" << std::endl;
                 std::cout << "3. Logout" << std::endl;
                 std::cin >> teachchoice;
 
                 if (teachchoice == 1) {
-                    Teacher1.getTimetable(); // calling function to get timetable
+                    //Teacher1.getTimetable(); // calling function to get timetable
                 } else if (teachchoice == 2) {
-                    Teacher1.getGrades(); // calling function to get students grades
+                    //Teacher1.getGrades(); // calling function to get students grades
                 } else if (teachchoice == 3) {
                     std::cout << "Logging out..." << std::endl; // logging out sequence
                     break;
@@ -59,9 +59,9 @@ int main() {
         // if they are a student
         if (idcheck > 15000) {
             int studchoice = 0;
-            Student Student1; // defining student type
+            //Student Student1; // defining student type
             while (studchoice != 1 || studchoice != 2 || studchoice != 3 || studchoice != 4 || studchoice != 5) {
-                std::cout << "Welcome " << Student1.getName() << std::endl; // giving options to the student
+                //std::cout << "Welcome " << Student1.getName() << std::endl; // giving options to the student
                 std::cout << "1. View Timetable" << std::endl;
                 std::cout << "2. View Grade" << std::endl;
                 std::cout << "3. Enrol" << std::endl;
@@ -70,18 +70,25 @@ int main() {
                 std::cin >> studchoice;
 
                 if (studchoice == 1) {
-                    Student1.getTimetable(); //calling function to get timetable
+                    //Student1.getTimetable(); //calling function to get timetable
                 } else if (studchoice == 2) {
-                    Student1.printGrades(); // calling function to get student's graes
+                    //Student1.printGrades(); // calling function to get student's graes
                 } else if (studchoice == 3) {
-                    Course Course1;
-                    std::string cname;
-                    std::cout << "Enter Course Name: ";
+                    int cname;
+                    std::cout << "Which Course would you like to enrol: " << std::endl;
+                    std::cout << "1. Maths" << std::endl;
+                    std::cout << "2. Chemistry" << std::endl;
+                    std::cout << "3. Physhics" << std::endl;
+                    std::cout << "4. OOP" << std::endl;
+                    std::cout << "5. Introduction To Engineering" << std::endl;
+                    std::cout << "6. Dynamics" << std::endl;
+                    std::cout << "7. Digital Electronics" << std::endl;
+                    std::cout << "8. Matlab and C" << std::endl;
+                    std::cout << "9. Introduction to Process Engineering" << std::endl;
+                    std::cout << "10. Analog Electrionics" << std::endl;
                     std::cin >> cname;
-                    Student1.enrol(&Course1);
                 } else if (studchoice == 4) {
-                    Course Course1;
-                    Student1.leaveCourse(&Course1);
+                    std::cout << "Which Course would you like to Remove: " << std::endl;
                 } else if (studchoice == 5) {
                     std::cout << "Logging out..." << std::endl; // logging out sequence
                     break;
@@ -97,22 +104,24 @@ int main() {
             std::cout << "1. Teacher" << std::endl;
             std::cout << "2. Student" << std::endl;
             std::cin >> role;
-        } if (role == 1) {
-            Teacher Teacher1;
-            std::string tname;
-            std::cout << "Please Enter your Name: ";
-            std::cin >> tname;
-            Teacher1.setName(tname); // storing teacher name in the teacher type
-            Teacher1.setIdNum(&teacher_id);
-            std::cout << "Your ID Number is : a" << Teacher1.getIdNum() << std::endl;
-        } else if (role == 2) {
-            Student Student1;
-            std::string tname;
-            std::cout << "Please Enter your Name: ";
-            std::cin >> tname;
-            Student1.setName(tname); // storing students name in the student type
-            Student1.setIdNum(&student_id);
-            std::cout << "Your ID Number is : a" << Student1.getIdNum() << std::endl;
+            if (role == 1) {
+                //Teacher Teacher1;
+                std::string tname;
+                std::cout << "Please Enter your Name: ";
+                std::cin >> tname;
+                //Teacher1.setName(tname); // storing teacher name in the teacher type
+                //Teacher1.setIdNum(&teacher_id);
+                //std::cout << "Your ID Number is : a" << Teacher1.getIdNum() << std::endl;
+            } else if (role == 2) {
+                //Student Student1;
+                std::string tname;
+                std::cout << "Please Enter your Name: ";
+                std::cin >> tname;
+                //Student1.setName(tname); // storing students name in the student type
+                //Student1.setIdNum(&student_id);
+                //std::cout << "Your ID Number is : a" << Student1.getIdNum() << std::endl;
+            }
+            break;
         }
     }
 
