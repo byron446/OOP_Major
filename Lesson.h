ce1e30daf511500306e4720358cc662f08987ae9
FLASH2719 <90818372+FLASH2719@users.noreply.github.com>
2021-10-14 13:45:41 +1030

    lesson 4

--- Lesson.h
+++ Lesson.h
@@ -33,19 +33,21 @@ private:
 
     void setLesson(LessonType plesson);
 
-    void getLesson();
+    std::string getLesson();
  
     int setDuration(int pduration);
 
     int getDuration();
 
    
-
+    std::string lesson;
 
 private:
     int lessonTime;
 
     int lessonDuration;
+
+    
 
   
     std::string lessonDate;
