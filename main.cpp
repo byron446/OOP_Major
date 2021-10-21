80de412a1b7055835bc89ebfe30e30af0a829edb
Byron <byronbettsrandom@gmail.com>
2021-10-21 12:17:11 +1030

    idk

--- main.cpp
+++ main.cpp
@@ -13,18 +13,12 @@
 // starter ID's created for teacher and student
 int teacher_id = 10000;
 int student_id = 15000;
-<<<<<<< HEAD
-std::vector <Student*> Students;
-Course zero_course;
-Teacher Teacher1[3];
-=======
 std::vector <Student*> students;
 std::vector <Teacher*> teachers;
 Course** courses;
 Course zero_course;
 Teacher Teacher1;
 Student Student1;
->>>>>>> 266fea19a5c2c3bcf7d0111b4201e92e9b951619
 int tindex = 0;
 int maxtindex = 0;
 int sindex = 0;
@@ -381,6 +375,7 @@ int main() {
     int course_total;
     std::cin >> course_total;
     courses = new Course*[course_total];
+    
 
     while (quit == 0) { // while loop to take user back to login page once finished
         quit = loginfunct(); // calling to login function
