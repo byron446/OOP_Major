b95178b102caea712d2028df813baa92954ced85
Max <maxasemenov@gmail.com>
2021-10-21 13:50:42 +1030

    commenting on course and lesson .cpp

--- Course.cpp
+++ Course.cpp
@@ -5,7 +5,7 @@
 #include <iostream>
 
 
-
+// default constructer
 Course::Course()
 {
     name = "?";
@@ -24,36 +24,43 @@ Course::Course(int lesson_num)
     course_id = "??";
 }
 
+// setting the name of course
 void Course::setName(std::string pname)
 {
     name = pname;
 }
 
+// getting the name of the course
 std::string Course::getName()
 {
     return name;
 }
 
+// setting the ID of the course
 void Course::setCourseID(std::string pid)
 {
     course_id = pid;
 }
 
+// getting the ID of the course
 std::string Course::getCourseId()
 {
     return course_id;
 }
 
+// adds a lesson to the course
 void Course::addLesson(Lesson* new_lesson)
 {
     lessons.push_back(new_lesson);
 }
 
+// returns the lesson within a course
 std::vector<Lesson*> &Course::getLessons()
 {
     return lessons;
 }
 
+// destructor
 Course::~Course()
 {
 
